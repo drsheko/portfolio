b22470e568255b9c37d9e3eb00cf4c3f6bd0b688
drsheko <drsheko@gmail.com>
2022-08-23 19:48:11 -0500

    add contact section

--- js/script.js
+++ js/script.js
@@ -4,12 +4,16 @@ const sideNav = document.querySelector('.sideNav')
 function displayNavNames  (){
     var navItemText = document.querySelectorAll('.nav-item-text')
     navItemText.forEach(ele=>{sideNav.style.width='150px';ele.style.display='block'})
-   
+    //console.log(sideNav.offsetWidth)
+   // const skills = document.querySelector('.skills')
+   // skills.style.marginLeft ='25%'
 }
 function hideNavNames  (){
     var navItemText = document.querySelectorAll('.nav-item-text')
     navItemText.forEach(ele=>{sideNav.style.width='50px';
      ele.style.display='none'
    } )
-   //sideNav.style.width = '10%'
+   //console.log(sideNav.offsetWidth)
+   //const skills = document.querySelector('.skills')
+   //skills.style.marginLeft =  '80px'
 }
